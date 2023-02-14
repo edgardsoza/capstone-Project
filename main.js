2c153d1a538c9c868507030659686a12115a548e
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-02-14 15:56:13 -0600

    finishing mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const toggle = document.querySelector('.menuicon');
-const weboptions = document.querySelector('.weboptions');
-const links = document.querySelectorAll('#menuweb');
+const toggle = document.getElementById('hamburger');
+const weboptions = document.querySelector('.mobile-bar');
+const links = document.querySelectorAll('.mobile-nav-link');
 
-toggle.addEventListener('click', () => weboptions.classList.toggle('active'));
+toggle.addEventListener('click', () => weboptions.classList.remove('display-none'));
 links.forEach((n) => n.addEventListener('click', () => {
-  weboptions.classList.remove('active');
+  weboptions.classList.add('display-none');
 }));
